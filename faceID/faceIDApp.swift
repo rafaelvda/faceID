4712cb0080067ee623e0eb3b6ef3ebddf1d00aeb
Rafael <rafaelvmico@gmail.com>
2023-11-16 14:48:50 +0100

    first commit

---
+++ faceID/faceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  faceIDApp.swift
+//  faceID
+//
+//  Created by Rafael Videira on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct faceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
